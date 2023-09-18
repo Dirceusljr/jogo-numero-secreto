0a28c2ee7ce4d1008399c643e2bbcfb6174b7b16
Dirceusljr <dirjunior18@gmail.com>
2023-09-18 18:52:36 -0300

    solução da Alura do desafio

--- app/validacao.js
+++ app/validacao.js
@@ -1,40 +1,19 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        if (chuteForInvalido(numero)) {
-            if (chute.toUpperCase() === "GAME OVER") {
-                document.body.innerHTML = `
-                
-                    <h2>Game Over!!!</h2>
-                    <h3>Pressione o botão para jogar novamente.</h3>
-        
-                    <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
-                
-                `
-                document.body.style.backgroundColor = "black"
-            } else {
-
-                elementoChute.innerHTML += '<div>Valor inválido</div>'
-            }
-=======
-=======
->>>>>>> 4ba832601ec17a70d41ab9a336a8b190c9fa5ab9
-    if (chute == "game over") {
-        document.body.innerHTML = `
-        <div class="game-over">
-            <h2>Game Over! O jogo acabou!</h2>
-            <h3>O número secreto era ${numeroSecreto}!</h3>
-
-            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
-        </div>
-        `
-    }
-
     if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML = `
+            
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente!</h3>
+    
+                <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+            `
+            document.body.style.backgroundColor = "black"
+        }
+
         elementoChute.innerHTML += '<div>Valor inválido</div>'
->>>>>>> 4ba832601ec17a70d41ab9a336a8b190c9fa5ab9
         return
     }
 
